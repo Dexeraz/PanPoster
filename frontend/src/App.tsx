07775ad7321e8a5c1c8632e25a711f7fb35fd955
MBilik <dexeraz@gmail.com>
2023-04-01 08:59:33 +0200

    error handling for submit button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import './App.css';
 import GroupForm from './components/GroupsForm';
 
 function App() {
--- frontend/src/components/GroupsForm.tsx
+++ frontend/src/components/GroupsForm.tsx
@@ -1,29 +1,39 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
 
 interface GroupFormProps {
-  onSubmit: (data: any) => void;
+  onSubmit: (data: any) => Promise<void>;
 }
 
 const GroupForm: React.FC<GroupFormProps> = ({ onSubmit }) => {
-  const [groupIds, setGroupIds] = useState('');
-  const [message, setMessage] = useState('');
-  const [accessToken, setAccessToken] = useState('');
+  const [groupIds, setGroupIds] = useState("");
+  const [message, setMessage] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    onSubmit({
-      groupIds: groupIds.split(',').map(id => id.trim()),
-      message,
-      accessToken,
-    });
+    if (!groupIds || !message || !accessToken) {
+      setStatusMessage("Please fill in all the fields.");
+      return;
+    }
+
+    try {
+      await onSubmit({
+        groupIds: groupIds.split(",").map((id) => id.trim()),
+        message,
+        accessToken,
+      });
+      setStatusMessage("Successfully posted to groups.");
+    } catch (error) {
+      setStatusMessage("Error posting to groups: " + error);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="groupIds">Group IDs (comma separated):</label>
+        <label htmlFor="groupIds">Group IDs (separated with comma):</label>
         <input
           type="text"
           id="groupIds"
@@ -32,7 +42,7 @@ const GroupForm: React.FC<GroupFormProps> = ({ onSubmit }) => {
         />
       </div>
       <div>
-        <label htmlFor="message">Message:</label>
+        <label htmlFor="message">Post content:</label>
         <textarea
           id="message"
           value={message}
@@ -49,6 +59,7 @@ const GroupForm: React.FC<GroupFormProps> = ({ onSubmit }) => {
         />
       </div>
       <button type="submit">Submit</button>
+      <p className="mt-4 text-white">{statusMessage}</p>
     </form>
   );
 };
